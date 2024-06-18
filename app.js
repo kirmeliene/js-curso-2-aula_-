8514dc2155417b2d3a466634eeb088c8ee535ebc
KIRMELIENE <wesley.kirmeliene@gmail.com>
2024-06-17 21:00:18 -0300

    Third

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
 
 
-let numeroSecreto =  gerarNumeroAleatorio();
+//let numeroSecreto =  gerarNumeroAleatorio();
+let numeroSecreto =  2;
 //let mensagemTentativas = `Você descobriu o número secreto com 5 tentativas!`;
 
   function exibirTextoNaTela(tag, texto) {
@@ -16,7 +17,7 @@ let numeroSecreto =  gerarNumeroAleatorio();
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
-
+let tentativas = 1;
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
@@ -25,19 +26,24 @@ function gerarNumeroAleatorio() {
 //     let chute = document.querySelector('input').value;
 //     console.log(numeroSecreto);
 //     console.log(chute == numeroSecreto);
-// }function verificarChute() {
+// }
+function verificarChute() {
         let chute = document.querySelector('input').value;
         
-        if (chute == numeroSecreto) 
+        if (chute == numeroSecreto) {
                 exibirTextoNaTela('h1', 'Acertou!');
                 let palavraTentativa = tentativas > 1 ? 'tentativas': 'tentativa';
                 let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`; 
                 exibirTextoNaTela('p', mensagemTentativas);
         } else {
+
                 if (chute > numeroSecreto) { exibirTextoNaTela ('p', '0 número secreto é menor');
                 } else {
                         exibirTextoNaTela('p', 'O número secreto é maior');
                 }
-                tentativas++;
+                tentativas++;                   
         }
-}
+
+
+        
+ }
